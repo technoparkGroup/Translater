8bf692fdc650497e1c8b9a470bbe27f22e344c64
Сергей <seregachern@mail.ru>
2014-10-02 16:24:37 +0400

    Parcelable LanguageElement + Correct sending language object to Translate activity

--- app/src/main/java/group/technopark/translater/activities/TranslateActivity.java
+++ app/src/main/java/group/technopark/translater/activities/TranslateActivity.java
@@ -3,9 +3,13 @@ package group.technopark.translater.activities;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import group.technopark.translater.R;
+import group.technopark.translater.adapters.LanguageElement;
+import group.technopark.translater.fragments.LanguagesList;
 import group.technopark.translater.fragments.TranslateFragmentFrom;
 import group.technopark.translater.fragments.TranslateFragmentTo;
 
@@ -13,6 +17,10 @@ public class TranslateActivity extends Activity implements FragmentController{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        Parcelable languageParcel = intent.getParcelableExtra(LanguagesList.SELECTED_LANGUAGE);
+        LanguageElement language =  (LanguageElement)languageParcel;
+        String code = language.getCode();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.translate_activity);
         if (savedInstanceState == null){
--- app/src/main/java/group/technopark/translater/adapters/LanguageElement.java
+++ app/src/main/java/group/technopark/translater/adapters/LanguageElement.java
@@ -1,14 +1,21 @@
 package group.technopark.translater.adapters;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class LanguageElement {
+public class LanguageElement implements Parcelable{
     private String title;
     private String code;
 
     public LanguageElement(String title, String code) {
         this.title = title;
         this.code = code;
+    }
+
+    public LanguageElement(Parcel parcel){
+        readFromParcel(parcel);
     }
 
     public String getTitle() {
@@ -27,5 +34,33 @@ public class LanguageElement {
         }
         return list;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(code);
+    }
+
+    public void readFromParcel(Parcel income){
+        title = income.readString();
+        code = income.readString();
+    }
+
+    public static final Parcelable.Creator<LanguageElement> CREATOR
+            = new Parcelable.Creator<LanguageElement>() {
+        public LanguageElement createFromParcel(Parcel in) {
+            return new LanguageElement(in);
+        }
+
+        public LanguageElement[] newArray(int size) {
+            return new LanguageElement[size];
+        }
+    };
+
 
 }
